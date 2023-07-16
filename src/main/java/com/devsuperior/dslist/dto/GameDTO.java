b9953bd4bc60a5e9247cbb737059d2517be50d09
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-07-16 13:29:42 -0300

    Get Game By Id

---
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -0,0 +1,69 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameDTO {
+
+	private Long id;
+	private String title;
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private String imgUrl;
+	private Double score;
+	private String shortDescription;
+	private String longDescription;
+
+	public GameDTO() {
+	}
+
+	public GameDTO(Game entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.year = entity.getYear();
+		this.genre = entity.getGenre();
+		this.platforms = entity.getPlatforms();
+		this.imgUrl = entity.getImgUrl();
+		this.score = entity.getScore();
+		this.shortDescription = entity.getShortDescription();
+		this.longDescription = entity.getLongDescription();
+		//BeanUtils.copyProperties(entity, this);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public String getPlatforms() {
+		return platforms;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+}
